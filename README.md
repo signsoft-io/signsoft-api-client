9a0429a4303166e12b0b4547b82ba30aa61c1579
Shawn Welch <101164+shrimpwagon@users.noreply.github.com>
2020-02-26 09:50:32 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# signsoft
-API wrapper
+# SignSoft API Client
+An API wrapper written with [![guzzle/guzzle](https://github.com/guzzle/guzzle) for communicating with SignSoft business sign software. This library is what is used to create the client portal. You can also use it to integrate into any pre-existing sign shop website or software system. Create customers, view orders, quotes, and saved designs!
+
+## Installation
+
+### With Composer
+
+```
+$ composer require signsoft-io/signsoft-api-client
+```
---
+++ src/SignSoft/SignSoftClient.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace SignSoft;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+
+class SignSoftClient {
+
+	const FINGERPRINT_HEADER = 'X-SignSoft-Fingerprint';
+
+	private $has_error = false;
+	
+	private $client;
+
+	public $fingerprint = null;
+
+	public function __construct($url, $key, $options = [])
+	{
+		$options = array_merge([
+			'base_uri' => $url,
+			'allow_redirects' => false,
+			'timeout' => 5.0,
+			'verify' => false,
+			'headers' => [
+				'Content-Type' => 'application/json',
+				'Accept' => 'application/json',
+				'Authorization' => 'Bearer ' . $key,
+			],
+		]);
+
+		$this->client = new Client($options);
+	}
+
+	public function get($endpoint, $data = [])
+	{
+		$options = [
+			'query' => $data,
+		];
+
+		return $this->request('GET', $endpoint, $options);
+	}
+
+	public function post($endpoint, $data = [])
+	{
+		$options = [
+			'json' => $data,
+		];
+
+		return $this->request('POST', $endpoint, $options);
+	}
+
+	public function put($endpoint, $data = [])
+	{
+		$options = [
+			'json' => $data,
+		];
+
+		return $this->request('PUT', $endpoint, $options);
+	}
+
+	public function patch($endpoint, $data = [])
+	{
+		$options = [
+			'json' => $data,
+		];
+
+		return $this->request('PATCH', $endpoint, $options);
+	}
+
+	public function delete($endpoint)
+	{
+		return $this->request('DELETE', $endpoint);
+	}
+
+	private function request($verb, $endpoint, $options = [])
+	{
+		$this->has_error = false;
+
+		$endpoint = 'api/' . $endpoint;
+
+		if( $this->fingerprint )
+		{
+			$options['headers'][self::FINGERPRINT_HEADER] = $this->fingerprint;
+		}
+
+		try
+		{
+			$response = $this->client->request($verb, $endpoint, $options);
+
+			$response = json_decode($response->getBody()->getContents());
+		}
+
+		catch(BadResponseException $e)
+		{
+			$this->has_error = true;
+
+			$response = json_decode($e->getResponse()->getBody()->getContents());
+
+			if( empty($response->errors) ) $response->errors = [$response->message];
+		}
+
+		return $response;
+	}
+
+	public function hasError()
+	{
+		return $this->has_error;
+	}
+}
